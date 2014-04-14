016d0cbedb120288698e4f76ad87fdcd8bce6fc2
Samuel Ryan <sam@samryan.co.uk>
2014-04-14 20:06:55 +0100

    embeddable widget (with twig templating) and json API

--- public/index.php
+++ public/index.php
@@ -2,16 +2,32 @@
 
 $router = new Klein\Klein();
 
-$router->respond('GET', '/[*:project].[json:format]?', function ($request, $response, $service)
+header('Access-Control-Allow-Origin: *');
+
+$router->respond('GET', '/[*:project].[json:format]?', function ($request, $response, $service, $app)
 {
 	$curse = new Widget\Curse(new Widget\CurseCrawler, new Widget\MemcacheCache(new Memcache));
 	$project = $curse->project($request->param('project'));
 
-	if ($request->param('format') == 'json')
-	{
-		if ( ! $project) return $response->code(404)->json(['error' => 'Project not found']);
-		return $response->json($project);
-	}
+	$latest = key(array_slice($project['versions'], 0, 1));
+
+	$version = $request->param('version', $latest);
+	if (empty($version)) $version = $latest;
+
+	if ( ! $project) return 404;
+	if ($request->param('format') == 'json') return $response->json($project);
+
+	return (new Twig_Environment(new Twig_Loader_Filesystem('../html')))
+		->render('widget.html', array_merge(
+			$project,
+			['version' => $version]
+		));
+});
+
+$router->respond('GET', '/', function ($request, $response)
+{
+	return (new Twig_Environment(new Twig_Loader_Filesystem('../html')))
+		->render('docs.html');
 });
 
 $router->dispatch();
