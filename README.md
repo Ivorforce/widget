2f5c66f5d18adc3d0a149633c65fb1f120dc1206
Samuel Ryan <sam@samryan.co.uk>
2014-04-14 21:19:23 +0100

    documentation and incl. widget themes

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ A simple widget and api for curse.com projects. Embed the widget to get a
 download button anywhere on the web, or use the API to retrieve project details
 for your own website!
 
-# Status
+# Contributing a widget theme
 
-in development
+Create a new HTML file in ```html/```, the name of the file is the key that will
+be used to specify the theme, for example ```citric.html``` would be accessible
+through ```widget.mcf.li/mc-mods/minecraft/mb-battlegear-2?theme=citric```.
+
+# How to use
+
+Visit [widget.mcf.li](http://widget.mcf.li) for project documentation or see
+```html/docs.html```.
--- public/index.php
+++ public/index.php
@@ -9,16 +9,17 @@ $router->respond('GET', '/[*:project].[json:format]?', function ($request, $resp
 	$curse = new Widget\Curse(new Widget\CurseCrawler, new Widget\MemcacheCache(new Memcache));
 	$project = $curse->project($request->param('project'));
 
-	$latest = key(array_slice($project['versions'], 0, 1));
-
-	$version = $request->param('version', $latest);
-	if (empty($version)) $version = $latest;
-
 	if ( ! $project) return 404;
 	if ($request->param('format') == 'json') return $response->json($project);
 
+	$latest = key(array_slice($project['versions'], 0, 1));
+	$version = $request->param('version', $latest);
+	if (empty($version)) $version = $latest;
+
+	$widget = $request->param('theme', 'widget');
+
 	return (new Twig_Environment(new Twig_Loader_Filesystem('../html')))
-		->render('widget.html', array_merge(
+		->render("{$widget}.html", array_merge(
 			$project,
 			['version' => $version]
 		));
