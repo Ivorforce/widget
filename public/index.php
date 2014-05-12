53b1f5150a384beefdaa3cc3258964a8c1754daa
Samuel Ryan <sam@samryan.co.uk>
2014-05-12 03:04:12 +0100

    Refactors the routing

--- public/index.php
+++ public/index.php
@@ -2,33 +2,45 @@
 
 $router = new Klein\Klein();
 
-header('Access-Control-Allow-Origin: *');
+$router->respond(function ($request, $response, $service, $app)
+{
+	$app->html = function ($view, $parameters = []) use ($response) {
+		$twig = new Twig_Environment(new Twig_Loader_Filesystem('../html'));
+		return $twig->render("{$view}.html", $parameters);
+	};
+
+	$app->json = function ($view, $parameters) use ($response) {
+		return $response->json($parameters);
+	};
+});
 
 $router->respond('GET', '/[*:project].[json:format]?', function ($request, $response, $service, $app)
 {
 	$curse = new Widget\Curse(new Widget\CurseCrawler, new Widget\MemcacheCache(new Memcache));
-	$project = $curse->project($request->param('project'));
 
-	if ( ! $project) return 404;
-	if ($request->param('format') == 'json') return $response->json($project);
+	$project = $curse->project($request->param('project'));
+	$format = $request->param('format', 'html');
+	$widget = $request->param('theme', 'widget');
+
+	if ( ! $project)
+	{
+		$response->code(404);
+		return $app->$format('404', ['error' => '404 Page Not Found']);
+	}
 
 	$latest = key(array_slice($project['versions'], 0, 1));
 	$version = $request->param('version', $latest);
 	if (empty($version)) $version = $latest;
 
-	$widget = $request->param('theme', 'widget');
-
-	return (new Twig_Environment(new Twig_Loader_Filesystem('../html')))
-		->render("{$widget}.html", array_merge(
-			$project,
-			['version' => $version]
-		));
+	return $app->$format($widget, array_merge(
+		$project,
+		['version' => $version]
+	));
 });
 
-$router->respond('GET', '/', function ($request, $response)
+$router->respond('GET', '/', function ($request, $response, $service, $app)
 {
-	return (new Twig_Environment(new Twig_Loader_Filesystem('../html')))
-		->render('docs.html');
+	return $app->html('docs');
 });
 
 $router->dispatch();
